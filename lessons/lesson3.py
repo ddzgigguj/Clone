dcbf4eaa9cc82ad6b48b6480d346231f467e2231
Azike200 <seregink161@gmail.com>
2023-08-14 14:53:51 +0600

    'HW3'

--- lessons/lesson3.py
+++ lessons/lesson3.py
@@ -3,29 +3,46 @@
 # 2 уровни защиты
 # 3 публичный,приватный,скрытый
 
+# сделать все атрибуты скрытыми
+# изучить что такое декаратор property
+# и применить его на все атрибуты
+
+
 class Bank:
     def __init__(self, name, age, money, password):
-        self.name = name
-        self.age = age
-        self._money = money
+        self.__name = name
+        self.__age = age
+        self.__money = money
         self.__passw = password
 
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @property
+    def money(self):
+        return self.__money
+
+    @property
+    def password(self):
+        return self.__passw
+
     def pname(self):
-        print(self.name, self._money)
+        print(self.name, self.money)
 
     def __ppas(self):
-        print(self.__passw)
+        print(self.password)
 
-    def pasww(self):
-        self.__ppas()
 
 beka = Bank('бека', 20, 0, '12345678987543')
-beka._money = 123456789
 
-print(beka._money)
 beka.pname()
+print(beka.age)
+print(beka.password)
 
-beka.__passw = '0'
-print(beka.__passw)
-beka.pasww()
-print(dir(Bank))
+
+
